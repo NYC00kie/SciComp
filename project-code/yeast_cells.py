e799864c9ce4d0e29645c4fd106342236c264829
Nici <nici.scheel@gmail.com>
2024-04-08 11:16:51 +0200

    changed way that yeast variables are stored

--- project-code/yeast_cells.py
+++ project-code/yeast_cells.py
@@ -12,29 +12,29 @@ def Metabolism(grid,cells,cellIdx):
 	# E: esidual constant that accounts for the amount of residual product (ethanol) per unit of metabolized glucose particle
 	#
 	#
-	U_max = cells[cellIdx][11]
-	I = cells[cellIdx][14]
-	Y = cells[cellIdx][15]
+	U_max = cells[11][cellIdx]
+	I = cells[14][cellIdx]
+	Y = cells[15][cellIdx]
 
 
 	# Nutriens Intake
 
-	Field_Glucose = grid[0][cells[cellIdx][0],cells[cellIdx][1]]//1
-	Field_Oxygen = grid[1][cells[cellIdx][0],cells[cellIdx][1]]//1
+	Field_Glucose = grid[0][cells[0][cellIdx],cells[1][cellIdx]]//1
+	Field_Oxygen = grid[1][cells[0][cellIdx],cells[1][cellIdx]]//1
 
 	z_1 = np.random.normal(loc=U_max, scale=0.2*U_max)
 
-	U = z_1 * c * np.pow(cells[cellIdx][3],2/3) * (1 - (K_1 * cells[cellIdx][4]))
+	U = z_1 * c * np.pow(cells[3][cellIdx],2/3) * (1 - (K_1 * cells[4][cellIdx]))
 
 	Eaten = np.min((U,Field_Glucose))
 
-	grid[0][cells[cellIdx][0],cells[cellIdx][1]] -= Eaten
+	grid[0][cells[0][cellIdx],cells[1][cellIdx]] -= Eaten
 
-	ME = I * cells[cellIdx][3] + K_2 * grid[2][cells[cellIdx][0],cells[cellIdx][1]] * c * np.pow(cells[cellIdx][3],2/3)
+	ME = I * cells[cellIdx][3] + K_2 * grid[2][cells[0][cellIdx],cells[1][cellIdx]] * c * np.pow(cells[cellIdx][3],2/3)
 
 	if ME > Eaten:
-		cells[cellIdx][16] += 1
-		if cells[cellIdx][16] >= cells[cellIdx][9]:
+		cells[16][cellIdx] += 1
+		if cells[16][cellIdx] >= cells[9][cellIdx]:
 			# The Cell has surpassed it maximum time without enough food.
 			# It now has died
 			cells[cellIdx] = np.zeros(len(cells[cellIdx]))
@@ -49,12 +49,12 @@ def Metabolism(grid,cells,cellIdx):
 		if Field_Oxygen - 6*Eaten >= 0:
 			# Perfect, the Oxygen cancelled out the Glucose, no Ethanol was produced
 			# remove Oxygen
-			grid[1][cells[cellIdx][0],cells[cellIdx][1]] += - 6 * Eaten
+			grid[1][cells[0][cellIdx],cells[1][cellIdx]] += - 6 * Eaten
 			# add CO_2
-			grid[3][cells[cellIdx][0],cells[cellIdx][1]] += 6 * Eaten
+			grid[3][cells[0][cellIdx],cells[1][cellIdx]] += 6 * Eaten
 		else:
 			# Oh No, the Oxygen wasnt enough to compensate the Glucose.
 			# How much Glucose was compensated ?
 			not_compensated = (Eaten - Field_Oxygen*1/6)
 			# for every glucose, 2 Ethanol will come from it
-			grid[2][cells[cellIdx][0],cells[cellIdx][1]] = 2 * not_compensated
+			grid[2][cells[0][cellIdx],cells[1][cellIdx]] = 2 * not_compensated
