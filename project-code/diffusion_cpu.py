9f0df429183c3e16b4e51efde5112b3a3675dcc6
Nici <nici.scheel@gmail.com>
2024-04-08 11:16:51 +0200

    changed way that yeast variables are stored

--- project-code/diffusion_cpu.py
+++ project-code/diffusion_cpu.py
@@ -26,9 +26,9 @@ def main_cpu():
     # CO_2
 
     grid[0] = np.random.uniform(0, 800, size=(width,height))
-    grid[1] = np.random.uniform(200, 800, size=(width,height))
+    grid[1] = np.random.uniform(600, 800, size=(width,height))
 
-    yeast_cells = np.random.rand(cells_n, 16)
+    yeast_cells = np.random.rand(16, cells_n)
 
     plt.imshow(grid[0])
     plt.colorbar()
