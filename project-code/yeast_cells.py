1f1f80bbe220c43f8644f5920614faa228a47316
Nici <nici.scheel@gmail.com>
2024-04-09 16:55:35 +0200

    fixed reproduction

--- project-code/yeast_cells.py
+++ project-code/yeast_cells.py
@@ -27,28 +27,18 @@ def metabolism(grid,cells,cellIdx):
 	Field_Oxygen = grid[1][int(cells[0][cellIdx]),int(cells[0][cellIdx])]
 	Field_Ethanole = grid[2][int(cells[0][cellIdx]),int(cells[0][cellIdx])]
 
-	print(Field_Glucose)
-	print(Field_Oxygen)
-	print(Field_Ethanole)
-
 	z_1 = np.random.normal(loc=U_max, scale=0.2*U_max)
 	z_2 = np.random.normal(loc=cells[cellIdx][16], scale=0.2*cells[cellIdx][16])
 	z_3 = np.random.normal(loc=cells[cellIdx][13], scale=0.2*cells[cellIdx][13])
-
-	print(z_1)
-	print(z_2)
-	print(z_3)
-
 	U = z_1 * np.power(cells[cellIdx][3],2/3) * (1 - (cells[cellIdx][12] * cells[cellIdx][4]) - z_2 * Field_Glucose)
 
-	print(U)
-
 	Eaten = np.min((U,Field_Glucose))
-	print(Eaten)
 
 	grid[0][int(cells[0][cellIdx]),int(cells[0][cellIdx])] -= Eaten
 
 	ME = I * cells[cellIdx][3] + Field_Ethanole * z_3 * np.power(cells[cellIdx][3],2/3)
+
+	print(f"[{(ME,Eaten)}]")
 
 	if ME > Eaten:
 		cells[cellIdx][10] += 1
@@ -56,11 +46,11 @@ def metabolism(grid,cells,cellIdx):
 			# The Cell has surpassed it maximum time without enough food.
 			# It now has died
 			cells[cellIdx] = np.zeros(cell_parameters)
-			return True;
 
 	else:
 		# The Cell has got enough food and can add to its mass
 		delta_m = Y * (U - ME)
+		print(f"{delta_m=}")
 		cells[cellIdx][3] += delta_m
 
 		# return ethanol if there was not enough oxygen to turn it all into water and CO_2
@@ -78,7 +68,6 @@ def metabolism(grid,cells,cellIdx):
             # should be correct as we work with molecular amounts
 			grid[2][int(cells[0][cellIdx]),int(cells[0][cellIdx])] = 2 * not_compensated
 
-	return False
 
 #@jit
 def reproduction(grid, cells, cellIdx):
@@ -90,18 +79,20 @@ def reproduction(grid, cells, cellIdx):
 		# Budding Phase 1
 
 		delta_m = cells[cellIdx][3] - cells[cellIdx][6]
-		if cells[cellIdx][7] <= delta_m:
+		print(f"minimal biomass growth, delta_m [{(cells[cellIdx][7],delta_m)}]")
+		if cells[cellIdx][7] >= delta_m:
 			# cell reached minimal budding mass for phase 2
 			cells[cellIdx][5] = 2
 
 	else:
-		# Budding Phase
+		# Budding Phase 2
 		delta_m = cells[cellIdx][3] - cells[cellIdx][6]
 		if 2 * cells[cellIdx][7] <= delta_m and cells[cellIdx][17] >= cells[cellIdx][8] :
 			# Cell division requirements are met.
 			# The time has come
 			# Execute Order 66
 
+			cells[cellIdx][5] = 1
 			cells[cellIdx][3] += - delta_m
 
 			cells[cellIdx][4] += 1
@@ -127,17 +118,15 @@ def reproduction(grid, cells, cellIdx):
 						0
 						]]
 
-			np.append(cells, babycell, axis=0)
+			cells = np.append(cells, babycell, axis=0)
 
 		elif cells[cellIdx][17] <= cells[cellIdx][8]:
 			# The Time has not come
 			cells[cellIdx][17] += 1
-
 	return cells
-
 #@jit
 def spread_cell(grid,cells,cellIdx):
-	d_x,d_y = np.random.randint(-1,1, size=2)
+	d_x,d_y = np.random.randint(-2,high=2, size=2)
 	cells[cellIdx][0] += d_x
 	cells[cellIdx][1] += d_y
 
@@ -146,11 +135,10 @@ def do_cell(grid, cells, cellIdx):
 
 	# load shared memory
 	if np.sum(cells[cellIdx]) == 0:
-		return cells,grid
+		return
 
-	if metabolism(grid,cells,cellIdx):
-		return cells,grid
+	metabolism(grid,cells,cellIdx)
 	cells = reproduction(grid,cells,cellIdx)
 	spread_cell(grid,cells,cellIdx)
 
-	return cells,grid
+	return cells
