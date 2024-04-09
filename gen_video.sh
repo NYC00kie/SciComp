e3b214100646a580666b7e258965d381110fcbbb
Nici <nici.scheel@gmail.com>
2024-04-09 17:21:50 +0200

    added video generation script

---
+++ gen_video.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+python3 -c 'import subprocess
+for i in range(4):
+        subprocess.call([
+        "ffmpeg",
+        "-framerate", "2",
+        "-i", f"grid_post_{i}_%d.jpg",
+        "-c:v", "libx264",
+        "-r", "2",
+        f"output_{i}.mp4",
+        "-y", "-hide_banner"
+        ])'
