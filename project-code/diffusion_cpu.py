3a643d563cae3857d9acbb42f7dfc91e4be8737f
Nici <nici.scheel@gmail.com>
2024-04-09 12:58:42 +0200

    fully did yeast cells doing stuff

--- project-code/diffusion_cpu.py
+++ project-code/diffusion_cpu.py
@@ -8,9 +8,12 @@ import math
 from multiprocessing import shared_memory, Process, Lock
 from multiprocessing import cpu_count, current_process
 from scipy.signal import convolve2d
+from yeast_cells import do_cell
 
 def main_cpu():
 
+
+    np.random.seed(0)
     # creating diffusion kernel
     p = 0.125
     kernel = np.ones((3, 3)) * p
@@ -18,12 +21,15 @@ def main_cpu():
 
     dim = (materials,width,height)
     
-    grid = numpy.zeros(dim, dtype=np.longdouble)
+    grid = np.zeros(dim,dtype=np.float64)
 
-    yeast_cells = np.zeros((cells_n, 17), dtype=np.longdouble)
+    #yeast_cells = np.zeros((cells_n, cell_parameters))
     
-    #yeast_base = [0, 0, 0, 1e-10, 0, 0, 1e-11, 2e-11, 2, 2, 0, 0.00000000001, 0.5, 0.5, 5e-13, 0.1, 0.1]
+    yeast_base = [[0, 0, 0, 1e-10, 0, 0, 1e-11, 2e-11, 2, 2, 0, 0.00000000001, 0.5, 0.5, 5e-13, 0.1, 0.1,0]]
     
+    yeast_cells = np.array(yeast_base)
+
+    print(len(yeast_cells))
     # Glucose
     # Sauerstoff
     # Ethanol
@@ -31,45 +37,48 @@ def main_cpu():
 
     grid[0] = np.random.uniform(0, 800, size=(width,height))
     grid[1] = np.random.uniform(600, 800, size=(width,height))
+  
+    """ von hier an ist die Reihenfolge der Schritte aus dem 'Paper
+    INDISIM-YEAST: an individual-based simulator on a website for 
+    experimenting and investigating diverse dynamics of yeast 
+    populations in liquid media' zu beachten:
 
-    yeast_cells = np.random.rand(cells_n,16)
-    
-    plt.imshow(grid[0])
-    plt.colorbar()
-    plt.savefig("grid_pre.jpg")
-    plt.clf()
-
-   """ von hier an ist die Reihenfolge der Schritte aus dem 'Paper
-   INDISIM-YEAST: an individual-based simulator on a website for 
-   experimenting and investigating diverse dynamics of yeast 
-   populations in liquid media' zu beachten:
-   
-   s-random motion
-   s-uptake of nutrient particles
-   s-enough nutrient particles for maintanance?
-   s-Enough nutrient particles for new biomass?
-   s-production and excretion of ethanole
-   -buding phase?
-   -cell division, new yeast cell (mutation!)
-   -unbudded phase
-   -requirments to be viable?
-   -update of new individual characteristics (wdym?)
-   -repeat
-   """
+    s-random motion
+    s-uptake of nutrient particles
+    s-enough nutrient particles for maintanance?
+    s-Enough nutrient particles for new biomass?
+    s-production and excretion of ethanole
+    -buding phase?
+    -cell division, new yeast cell (mutation!)
+    -unbudded phase
+    -requirments to be viable?
+    -update of new individual characteristics (wdym?)
+    -repeat
+    """
 
     for i in range(iterations):
-        if i % 100 == 0:
-            print(np.sum(grid))
-        
+        if i % 1 == 0:
+            print(f"Glucose:{np.sum(grid[0])}")
+            print(f"Oxy:{np.sum(grid[1])}")
+            print(f"Ethanol:{np.sum(grid[2])}")
+            print(f"CO_2:{np.sum(grid[3])}")
+            print(f"Cells:{len(yeast_cells)}")
+
+        # diffuse material
         for entry in range(materials):
 
-            grid[entry] = convolve2d(grid[entry], kernel, mode="same", boundary="wrap")
+            if i % 1 == 0:
+                plt.imshow(grid[entry])
+                plt.colorbar()
+                plt.savefig(f"grid_post_{entry}_{i}.jpg")
+                plt.clf()
 
-            if i % 10 == 0:
-                    plt.imshow(grid[entry])
-                    plt.colorbar()
-                    plt.savefig(f"grid_post_{entry}_{i//10}.jpg")
-                    plt.clf()
+
+            grid[entry] = convolve2d(grid[entry], kernel, mode="same", boundary="wrap")        
+
+        # do the cell, yes I said it.
+        for i in range(len(yeast_cells)):
+            yeast_cells,grid = do_cell(grid,yeast_cells,i)
 
 
 
