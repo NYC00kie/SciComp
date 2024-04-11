254b163717c1255f5188cc5cfbe92312d1154377
Nici <nici.scheel@gmail.com>
2024-04-11 16:35:13 +0200

    readded higher limit

--- project-code/diffusion_cpu.py
+++ project-code/diffusion_cpu.py
@@ -87,7 +87,7 @@ def main_cpu():
     1:[]
     }
     print(iterations)
-    with Pool() as p:
+    with Pool(32) as p:
         for i in range(iterations):
             if i % loggingit == 0:
                 print(f"Glucose:{np.sum(grid[0])}")
