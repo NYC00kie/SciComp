106a056526986d2753bd58a6dec9c48318aaf0b0
Nici <nici.scheel@gmail.com>
2024-04-11 13:40:48 +0200

    really did the merge now

--- project-code/KONSTANTS.py
+++ project-code/KONSTANTS.py
@@ -2,13 +2,9 @@
 
 cells_n = 1
 cell_parameters = 19
-<<<<<<< HEAD
 iterations = 50
-=======
-iterations = 4000
->>>>>>> master
 materials = 4
-width = 1000
+width = 1000       
 height = 1000
 
 # Debugging Constants
