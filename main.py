414644532fa2d3b1f94f3d383d24d0f5aed1086a
Nici <nici.scheel@gmail.com>
2024-06-14 17:06:43 +0200

    added multiple itterations

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import sys
 sys.path.insert(0, './project-code')
 from diffusion_cpu import main_cpu
 
+itterations = 10
+
 if __name__ == '__main__':
-	main_cpu()
+	for i in range(itterations):
+		main_cpu()
