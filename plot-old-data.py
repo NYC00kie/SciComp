949b92643243a7df24a7329a9c10c07b6f2a7a41
Nici <nici.scheel@gmail.com>
2024-06-18 15:28:38 +0200

    some weird fucking data after doing 10 itterations and using the last suriving cell from each

---
+++ plot-old-data.py
@@ -0,0 +1,16 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+datenliste = [open(f"10-itterations-daten/params-{i}.csv", "r").readlines() for i in range(1,11)]
+
+daten = datenliste[0]
+
+for paramindex in range(6):
+	for datenlisteindex in range(10):
+		parami = np.array([float(data) for data in datenliste[datenlisteindex][paramindex].split(",")])
+		plt.plot(parami,label=f"itt. {datenlisteindex}")
+
+	plt.legend()
+	plt.savefig(f"10-itterations-daten/cell_params_{paramindex}")
+	plt.clf()
